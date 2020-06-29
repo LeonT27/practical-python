428d4f72a6262a230a0ecc7b56dcb98da6598d94
Luis Tolentino <luistolentino27@gmail.com>
2020-06-29 18:11:19 -0400

    1.5

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+current_height = 100
+bounces = 3/5
+total_bounces = 10
+current_bounce = 0
+
+while current_bounce < total_bounces:
+    current_height *= bounces
+    current_bounce += 1
+    print(current_bounce, round(current_height,4))
